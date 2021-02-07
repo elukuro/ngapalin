9eee71b77e2a209b3c002544fe66309eeb11faf1
elukuro <heruhartantot@gmail.com>
2021-02-07 18:55:30 +0700

    Add env files and render surah name list

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = {
     getSurah:async function() {
-        return await axios.get('https://api.quran.sutanlab.id/surah').then((response) =>{
+        return await axios.get(`${process.env.REACT_APP_API_URL}/surah`).then((response) =>{
             return response.data
         })
     }
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,23 +1,27 @@
-import React, {useState,useEffect} from 'react';
-import API from './../api/index.js';
+import { render } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
+import API from "./../api/index.js";
 
-function Home () {
-    const [surah, setSurah] = useState(); 
-    useEffect(()=>{
-        API.getSurah().then(function(result){
-            setSurah(result.data)
-        });
-    })
-    if(surah){
-       return(
-           <p>{JSON.stringify(surah)}</p>
-       )
-    }else{
-        return(
-            <p>loading...</p>
-        )
-    }
-    
+function renderSurah(surah) {
+  return(
+    <div>
+      {surah.map((item) => {return(<div>{item.name.transliteration.id}</div>)})}
+    </div>
+  )
+}
+
+function Home() {
+  const [surah, setSurah] = useState();
+  useEffect(() => {
+    API.getSurah().then(function (result) {
+      setSurah(result.data);
+    });
+  },[]);
+  if (surah) {
+    return renderSurah(surah)
+  } else {
+    return <p>loading...</p>;
+  }
 }
 
 export default Home;
