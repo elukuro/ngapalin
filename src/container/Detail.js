4eaa58c559a064c62ae1471210361ecc88069d60
elukuro <heruhartantot@gmail.com>
2021-02-22 09:01:59 +0700

    Add blur toggle

--- src/container/Detail.js
+++ src/container/Detail.js
@@ -9,14 +9,22 @@ import UiArrow from "./../components/Arrow";
 function Detail() {
   const [selectedSurah, setSurahList] = useState();
   const [renderSurah,setRenderSurah] = useState();
+  const [isHide,setHide] = useState(false);
 
   let { id,ayat } = useParams();
   const nextAyat = parseInt(ayat)+1;
   const prevAyat = parseInt(ayat) -1;
+
+  const toggleHide = function(isHide) {
+    setHide(!isHide)
+  };
+
   const filter = function (data, payload = id) {
     return data.filter((data) => data.id === parseInt(payload));
   };
+
   useEffect(() => {
+    setRenderSurah();
     let localSurah  = localStorage.getItem('surah');
     setSurahList(filter(JSON.parse(localSurah)));
     // render detail surah 
@@ -24,19 +32,20 @@ function Detail() {
       setRenderSurah(result.data);
     });
   }, [ayat]);
+
   if (renderSurah) {
     return (
       <div className="px-4">
         <UiArrow to={"/"} type="prev" text="Kembali pilih surat" />
         <UiStep step="3" />
         <p className="text-center container mx-auto font-extralight text-xl my-5">
-          Baca surat <span className="font-normal">{selectedSurah[0].surat_name}</span> ayat <span className="font-normal">{ayat}</span>
+          Baca surat <span className="font-normal text-gray-700">{selectedSurah[0].surat_name}</span> ayat <span className="font-normal text-gray-700">{ayat}</span>
         </p>
         <p className="text-center container mx-auto font-extralight text-base my-5">
           *Ulangi bacaan beberapa kali
         </p>
         <div className="text-center container mx-auto my-20">
-          <p className="text-4xl ayat">
+          <p className={`text-4xl ayat text-gray-500 ${(isHide) ? "ayat--blur" : ""}` }>
             {
               renderSurah[0].aya_text
             }
@@ -44,9 +53,11 @@ function Detail() {
           <div className="text-base font-extralight my-5" dangerouslySetInnerHTML={{__html:renderSurah[0].translation_aya_text }}></div>
         </div>
         <div className="container text-center mx-auto my-10">
-          <span className="rounded-full	py-3 px-6 border-gray-700 bg-gray-200">Sembunyikan</span>
+          <span className={`rounded-full py-3 px-6 ${(isHide) ? "bg-gray-200" : "bg-gray-700 text-white"}`} onClick={()=> toggleHide(isHide)}>
+            {isHide ? 'Tampilkan 😊' :'Sembunyikan 😅'}
+          </span>
         </div>
-        <div className="container mt-20 mb-2 flex justify-between">
+        <div className="container mt-20 mb-4 flex justify-between">
           {(id !== null && ayat > 1) ? <UiArrow to={`/surah/${id}/${prevAyat}`} type="prev" text="Ayat Sebelumnya" /> :''}
           {(id !== null && nextAyat <= parseInt(selectedSurah[0].count_ayat)) ? <UiArrow to={`/surah/${id}/${nextAyat}`} type="next" text="Ayat Selanjutnya" /> :''}
         </div>
