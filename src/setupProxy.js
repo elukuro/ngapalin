30daf4146cab22667ccf91412bd16d1d089923dd
elukuro <heruhartantot@gmail.com>
2021-02-27 09:42:37 +0700

    Update setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = app => {
     "/api",
     createProxyMiddleware({
       target: `${process.env.REACT_APP_API_URL}`,
-      changeOrigin: true
+      changeOrigin: true,
     })
   );
 };
